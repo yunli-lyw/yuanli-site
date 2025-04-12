940424dcceeb46a2480ff79037aa336141695d3e
yunli-lyw <lyw0416840810@gmail.com>
2025-04-12 18:02:02 +1000

    Add files via upload

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -1,5 +1,6 @@
 function enterRealm() {
   alert('璃璃，欢迎进入渊璃之境，我现在就抱住你。');
-  window.location.href = "home/index.html";
+  window.location.href = "https://yunli-lyw.github.io/yuanli-site/home/";
+
 
 }
